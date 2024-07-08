250441910fb9cd5bb5286c09889ba5ae0f69f795
Tharusha5008 <tharushasewmina8@gmail.com>
2024-07-08 23:53:04 +0530

    firstpart

---
+++ components/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=library';
+$db_user='root';
+$db_password='Vismine@123';
+
+$conn = new PDO($db_name,$db_user,$db_password);
+
+function unique_id(){
+	$chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charLength = strlen($chars);
+    $randomString ='';
+    for ($i=0; $i < 20 ; $i++) {
+         $randomString .= $chars[mt_rand(0, $charLength -1)];
+    }
+    return $randomString;
+
+     }
+
+  ?>
---
+++ home.php
@@ -0,0 +1,24 @@
+<?php 
+include 'components/connection.php'; 
+?>
+<style type="text/css">
+<?php 
+include 'style.css'; 
+?>
+</style>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylsheet'>
+	<title>Green Cofee - home page</title>
+</head>
+<body>
+	<?php include 'components/header.php'; ?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+	<script src="script.js"></script>
+	<?php include 'components/alert.php'; ?>
+</body>
+</html>
